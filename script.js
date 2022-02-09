1bc3ea1d7602a2d29d63db8291c716db657ff7a2
Austin Holeman <austinholeman@gmail.com>
2022-02-08 18:29:30 -0800

    button event added

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
 
+function showPicture() {
+    var sourceOfPicture = "images/raccoon.png";
+    var img = document.getElementById('raccoon')
+    img.src = sourceOfPicture.replace('90x90', '225x225');
+    img.style.display = "block";
+  }  
